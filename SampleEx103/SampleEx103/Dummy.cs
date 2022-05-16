c40e38c6ec5ab49ac56945b67a0b79c35058c54a
神原 茉里 <maacrei5723@gmail.com>
2022-05-16 10:48:19 +0900

    応用編１日目まで

---
+++ SampleEx103/SampleEx103/Dummy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SampleEx103
+{
+    public class Dummy
+    {
+        //　コンストラクタ
+        public Dummy()
+        {
+            Console.WriteLine("コンストラクタ");
+        }
+        //　デストラクタ
+        ~Dummy()
+        {
+            Console.WriteLine("デストラクタ");
+        }
+    }
+}
---
+++ SampleEx103/SampleEx103/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleEx103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dummy d = new Dummy();
+        }
+    }
+}
